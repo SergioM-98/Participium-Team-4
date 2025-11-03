b54168c767dae670b566cb08dae033c7b9fc7f56
Danish <danishaliq18@gmail.com>
2025-11-03 12:13:16 +0100

    basic setup

---
+++ src/app/components/registrationForm.tsx
@@ -0,0 +1,7 @@
+export default function registraionForm() {
+  return (
+    <div className="flex flex-col items-center py-2">
+      <h1 className="text-3xl font-bold underline"> Registration form! </h1>
+    </div>
+  );
+}
