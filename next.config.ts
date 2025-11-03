39fc8584bd981c9b29aa3fb277bfe62378be5712
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-11-03 15:48:59 +0100

    feat: add bodySizeLimit configuration for server actions in Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb', // Aumenta il limite se necessario
+    },
+  },
 };
+
+
 
 export default nextConfig;
