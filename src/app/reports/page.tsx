605a02719a496f11265d044b573535534cbbff7b
Skeitt <benevengafilippo@gmail.com>
2025-11-03 17:49:39 +0100

    feature: - configuration of shadcn - added Navbar1 element to the layout file - added reports page (unimplemented)

---
+++ src/app/reports/page.tsx
@@ -0,0 +1,7 @@
+export default function ReportsPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-[60vh]">
+      <h1 className="text-3xl font-bold mb-4">Reports</h1>
+    </main>
+  );
+}
