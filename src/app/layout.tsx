231bd7510b1e888b7e94f379e07e74b1ad06f921
Danish Ali Qureshi <147446159+danishaliq@users.noreply.github.com>
2025-11-07 11:06:02 +0100

    Revert "Feature/#14 pt05 gui"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Navbar1 } from "@/components/navbar1"; 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar1 />
         {children}
       </body>
     </html>
