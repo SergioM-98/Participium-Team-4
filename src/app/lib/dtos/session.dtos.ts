e7fbe872488f32d0a28ec1cd18c970660444fc0f
Gabriele Agosta <g.agosta02@gmail.com>
2025-11-07 10:37:20 +0100

    fix: Changes to DTOs to accomodate GUI

--- src/app/lib/dtos/session.dtos.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const SessionPayloadSchema = z.object({
-  id: z.bigint(),
-  role: z.string().optional(),
-  expiresAt: z.number(),
-});
-
-export type SessionPayload = z.infer<typeof SessionPayloadSchema>;
