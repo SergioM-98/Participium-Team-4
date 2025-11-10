584cff28085bdc9ce583729ec4312d6adaa5b7d9
Skeitt <benevengafilippo@gmail.com>
2025-11-10 23:41:38 +0100

    fix: - fixed jest setup and added migrations

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,6 @@
 // jest.setup.js
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const dotenv = require('dotenv');
+const path = require('path');
 
 // Carica le variabili d'ambiente per i test
 dotenv.config({ path: path.join(__dirname, '.env.test') });
