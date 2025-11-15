bb0f3d03c53d75b381ae08ab50a4d4a9307891f1
Gabriele Agosta <g.agosta02@gmail.com>
2025-11-15 14:02:03 +0100

    feat: Implemented report retrieval for single officer

--- src/app/lib/actions/report.ts
+++ src/app/lib/actions/report.ts
@@ -1,46 +1,58 @@
-'use server'
+"use server";
 
-import { ReportRegistrationResponse, reportRequestSchema } from '@/dtos/report.dto';
-import { ReportController } from '@/controllers/report.controller';
+import {
+  ReportRegistrationResponse,
+  reportRequestSchema,
+} from "@/dtos/report.dto";
+import { ReportController } from "@/controllers/report.controller";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/auth";
 
 export async function createReport(
-    title: string, 
-    description: string, 
-    photos: string[], 
-    category: string, 
-    longitude: number, 
-    latitude: number, 
-    userId: string,
-    isAnonymous: boolean
+  title: string,
+  description: string,
+  photos: string[],
+  category: string,
+  longitude: number,
+  latitude: number,
+  userId: string,
+  isAnonymous: boolean
 ): Promise<ReportRegistrationResponse> {
+  const session = await getServerSession(authOptions);
 
-    const session = await getServerSession(authOptions);
+  const reportData = reportRequestSchema.safeParse({
+    title,
+    description,
+    photos,
+    category: category,
+    longitude,
+    latitude,
+    userId,
+    isAnonymous,
+  });
 
-    const reportData = reportRequestSchema.safeParse({
-        title,
-        description,
-        photos,
-        category: category,
-        longitude,
-        latitude,
-        userId,
-        isAnonymous
-    });
+  if (!reportData.success) {
+    return {
+      success: false,
+      error: "Invalid inputs",
+    };
+  }
 
-    if(!reportData.success){
-        return {
-            success: false,
-            error: "Invalid inputs"
-        }
-    }
+  if (!session || (session && session.user.role !== "CITIZEN")) {
+    return { success: false, error: "Unauthorized report" };
+  }
 
-    if(!session || (session && session.user.role !== "CITIZEN")){
-        return { success: false, error: "Unauthorized report" };
-    }
+  const reportController = new ReportController();
+  return await reportController.createReport(reportData.data);
+}
 
-    const reportController = new ReportController();
-    return await reportController.createReport(reportData.data);
-    
+export async function retrieveReportsByOfficerId() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || (session && session.user.role !== "OFFICER")) {
+    return { success: false, error: "Unauthorized access" };
+  }
+
+  const reportController = new ReportController();
+  return await reportController.getReportsByOfficerId(Number(session.user.id));
 }
--- src/app/lib/controllers/report.controller.ts
+++ src/app/lib/controllers/report.controller.ts
@@ -1,12 +1,23 @@
-import { ReportRegistrationResponse, ReportRequest } from '@/dtos/report.dto';
-import { ReportCreationService } from '@/services/reportCreation.service';
+import {
+  ReportRegistrationResponse,
+  ReportRequest,
+  ReportsByOfficerIdResponse,
+} from "@/dtos/report.dto";
+import { ReportCreationService } from "@/services/reportCreation.service";
+import { ReportRetrievalService } from "../services/reportRetrieval.service";
 
 class ReportController {
+  async createReport(data: ReportRequest): Promise<ReportRegistrationResponse> {
+    const reportCreationService = ReportCreationService.getInstance();
+    return reportCreationService.createReport(data);
+  }
 
-    async createReport(data: ReportRequest): Promise<ReportRegistrationResponse> {
-        const reportCreationService = ReportCreationService.getInstance();
-        return reportCreationService.createReport(data);
-    }
+  async getReportsByOfficerId(
+    officerId: number
+  ): Promise<ReportsByOfficerIdResponse> {
+    const reportRetrievalService = ReportRetrievalService.getInstance();
+    return reportRetrievalService.retrieveReportsByOfficerId(officerId);
+  }
 }
 
 export { ReportController };
