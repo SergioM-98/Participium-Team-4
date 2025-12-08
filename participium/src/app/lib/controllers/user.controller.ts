f31d6a1563a8555c74ccde77dc45b3f809276f78
Andrea Maria Spina s334214 <s334214@studenti.polito.it>
2025-12-08 16:27:22 +0100

    refactor: update registration logic to restrict unauthorized access and improve role validation

--- participium/src/app/lib/controllers/user.controller.ts
+++ participium/src/app/lib/controllers/user.controller.ts
@@ -34,8 +34,6 @@ export async function register(
 ): Promise<RegistrationResponse> {
   const session = await getServerSession(authOptions);
 
-  //throw new Error(formData.get("role")?.toString());
-
   const validatedData = RegistrationInputSchema.safeParse({
     id: crypto.randomUUID(),
     firstName: formData.get("firstName"),
@@ -63,7 +61,7 @@ export async function register(
   }
 
   if (session || (!session && !validatedData.data?.role.includes("CITIZEN"))) {
-    if (session?.user.role.includes("ADMIN")) {
+    if (!session?.user.role.includes("ADMIN")) {
       console.error("Unauthorized registration attempt by user:", session?.user.username);
       return { success: false, error: "Unauthorized registration" };
     }
